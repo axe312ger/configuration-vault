4cb1c75d6f50e4226752bae26086012a1225b6aa
Benedikt Rötsch <info@benedikt-roetsch.de>
2016-04-01 22:54:34 +0200

    adds missing custom binaries folder

--- setup-configuration-vault.sh
+++ setup-configuration-vault.sh
@@ -3,4 +3,8 @@
 ## Init submodules. Often forgotten, but important!
 git submodule update --init --recursive
 
+## Create folder for custom binaries
 mkdir ~/bin
+
+## Install command line tools
+gcc
